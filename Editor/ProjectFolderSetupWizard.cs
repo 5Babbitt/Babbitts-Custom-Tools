1691871439e43c0b459325f6d54ff4efbb2a8199
Owen Harbert <owen.harbert2@gmail.com>
2024-02-12 18:01:37 +0200

    Use AssetDatabase to copy the input asset file the use fileutil to delete them

--- Editor/ProjectFolderSetupWizard.cs
+++ Editor/ProjectFolderSetupWizard.cs
@@ -47,7 +47,7 @@ namespace Babbitt.Tools.Editor
         public bool UseNamespace = false;
         private string SFGUID;
 
-        string packageInputPath = "Packages/com.5babbittgames.babbitts-custom-tools/Runtime/Input/GameInput.inputactions";
+        string packageInputPath = "Packages/com.5babbittgames.babbitts-custom-tools/Runtime/Input";
 
         public List<string> nsFolders = new List<string>();
         public List<string> folders = new List<string>() { "Scenes", "_Scripts", "Animation", "Audio", "Materials", "Meshes", "Prefabs", "Resources", "Textures", "Sprites", "Input", "GameEvents" };
@@ -132,8 +132,10 @@ namespace Babbitt.Tools.Editor
         // Moves the existing Input Actions Asset to the created Input folder
         void InitializeInputFolder(string path)
         {
-            //AssetDatabase.CopyAsset(packageInputPath, path + "/GameInput.inputactions");
-            FileUtil.MoveFileOrDirectory(packageInputPath, path + "/GameInput.inputactions");
+            AssetDatabase.CopyAsset(packageInputPath, path + "/GameInput.inputactions");
+            //FileUtil.MoveFileOrDirectory(packageInputPath, path + "/GameInput.inputactions");
+            FileUtil.DeleteFileOrDirectory(packageInputPath + "/GameInput.inputactions");
+            FileUtil.DeleteFileOrDirectory(packageInputPath + "/GameInput.cs");
             //FileUtil.MoveFileOrDirectory(packageInputPath, path + "/GameInput.cs");
 
             //AssetDatabase.CopyAsset("Packages/Babbitts-Custom-Tools/Runtime/Input/GameInput.inputactions", path + "/GameInput.inputactions");
