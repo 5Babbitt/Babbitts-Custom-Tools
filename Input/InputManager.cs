ff6f1857aed805684cec5c16f72e8d3e1c947b0f
Owen Harbert <owen.harbert2@gmail.com>
2024-02-23 20:11:42 +0200

    Removed Input System

--- Input/InputManager.cs
+++
@@ -1,7 +0,0 @@
-using Babbitt.Tools;
-
-public class InputManager : Singleton<InputManager>
-{
-    // This class exists solely to always have a reference to the input reader throughout the game
-    public InputReader inputReader;
-}
