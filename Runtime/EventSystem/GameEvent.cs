f12895cfc8c051073b36b6a6ae2b8b8f2fb8b11f
Owen Harbert <owen.harbert2@gmail.com>
2024-02-28 20:06:08 +0200

    Update Game Event Script

--- Runtime/EventSystem/GameEvent.cs
+++ Runtime/EventSystem/GameEvent.cs
@@ -20,6 +20,21 @@ namespace FiveBabbittGames
             Raise(sender, null);
         }
 
+        public void Raise(string data)
+        {
+            Raise(null, data);
+        }
+
+        public void Raise(int data)
+        {
+            Raise(null, data);
+        }
+
+        public void Raise(float data)
+        {
+            Raise(null, data);
+        }
+
         public void Raise(Component sender, object data) {
             for (int i = listeners.Count -1; i >= 0; i--) {
                 listeners[i].OnEventRaised(sender, data);
