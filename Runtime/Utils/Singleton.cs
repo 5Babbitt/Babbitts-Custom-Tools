4c41a26be5ec47af5fb1c220b998be33fab7f6cb
Owen Harbert <owen.harbert2@gmail.com>
2024-03-16 23:09:01 +0200

    Edit Singleton

--- Runtime/Utils/Singleton.cs
+++ Runtime/Utils/Singleton.cs
@@ -1,36 +1,30 @@
 using UnityEngine;
 
-
 namespace FiveBabbittGames
 {
-    // A static instance is similar to a singleton but does not destroy any new instances,
-    // Instead overwriting the current existing instance.
-    // Good for resetting states.
-    public abstract class StaticInstance<T> : MonoBehaviour where T : MonoBehaviour
+    // A Basic Singleton. Will destroy any new instances created.
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
         public static T Instance { get; private set; }
-        protected virtual void Awake() => Instance = this as T;
+
+        protected virtual void Awake()
+        {
+            if (Instance == null)
+                Instance = this as T;
+
+            if (Instance != null && Instance != this) 
+            {
+                Destroy(gameObject);
+                return;
+            }
+        }
 
         protected virtual void OnApplicationQuit()
         {
             Instance = null;
-            Destroy(this);
+            Destroy(gameObject);
         }
-    }
 
-    // A Basic Singleton. Will destroy any new instances created.
-    public abstract class Singleton<T> : StaticInstance<T> where T : MonoBehaviour
-    {
-        protected override void Awake()
-        {
-            if (Instance != null && Instance != this) 
-            {
-                Destroy(this);
-                return;
-            }
-
-            base.Awake();
-        }
     }
 
     // Persistant version of a singleton. Will not be destroyed when loading a new scene.
