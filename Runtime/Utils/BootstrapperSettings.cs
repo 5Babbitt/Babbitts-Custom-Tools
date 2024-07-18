1f30f46dfd259443183a7e1bc4a54c6afb50414e
Owen Harbert <owen.harbert2@gmail.com>
2024-07-18 02:34:11 +0200

    Update BootstrapperSettings.cs

--- Runtime/Utils/BootstrapperSettings.cs
+++ Runtime/Utils/BootstrapperSettings.cs
@@ -2,7 +2,7 @@
 
 namespace FiveBabbittGames
 {
-    [CreateAssetMenu(fileName = "BootstrapperSettings", menuName = "Settings/BootstrapperSettings")]
+    [CreateAssetMenu(menuName = "Settings/BootstrapperSettings")]
     public class BootstrapperSettings : ScriptableObject
     {
         public bool runFromBootsrapperScene = false; // change this value when building the project
