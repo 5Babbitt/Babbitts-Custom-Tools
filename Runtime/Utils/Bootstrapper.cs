ff6d4107ddcbc944d69825b5b4200c515be2ff9d
Owen Harbert <owen.harbert2@gmail.com>
2024-07-18 02:38:40 +0200

    Add meta file

--- Runtime/Utils/Bootstrapper.cs
+++ Runtime/Utils/Bootstrapper.cs
@@ -5,15 +5,15 @@ namespace FiveBabbittGames
 {
     public class Bootstrapper : Singleton<Bootstrapper>
     {
-        //static BootstrapperSettings settings = Resources.FindObjectsOfTypeAll<BootstrapperSettings>()[0];
+        static BootstrapperSettings settings = Resources.FindObjectsOfTypeAll<BootstrapperSettings>()[0];
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         public static async void Init()
         {
             DontDestroyOnLoad(Instantiate(Resources.Load("Systems")));
 
-            //if (settings.runFromBootsrapperScene)
-            //    await SceneManager.LoadSceneAsync("Bootstrapper", LoadSceneMode.Single);
+            if (settings.runFromBootsrapperScene)
+                await SceneManager.LoadSceneAsync("Bootstrapper", LoadSceneMode.Single);
         }
     }
 }
