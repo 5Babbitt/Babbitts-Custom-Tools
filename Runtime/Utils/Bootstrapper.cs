70655e3a4e38f611a2057c11a90ae0f070e1194d
Owen Harbert <owen.harbert2@gmail.com>
2024-07-31 09:04:58 +0200

    Update Bootstrapper.cs

--- Runtime/Utils/Bootstrapper.cs
+++ Runtime/Utils/Bootstrapper.cs
@@ -5,12 +5,14 @@ namespace FiveBabbittGames
 {
     public class Bootstrapper : Singleton<Bootstrapper>
     {
-        static BootstrapperSettings settings = Resources.FindObjectsOfTypeAll<BootstrapperSettings>()[0];
+        static BootstrapperSettings settings;
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         public static async void Init()
         {
             DontDestroyOnLoad(Instantiate(Resources.Load("Systems")));
+
+            settings = Resources.FindObjectsOfTypeAll<BootstrapperSettings>()[0];
 
             if (settings.runFromBootsrapperScene) // Create Asset in the Resources Directory if this throws and error
                 await SceneManager.LoadSceneAsync("Bootstrapper", LoadSceneMode.Single);
