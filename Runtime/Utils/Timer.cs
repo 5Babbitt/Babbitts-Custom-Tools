306d826933e671bbb5bbdb9cbe5288644c06c217
Owen Harbert <owen.harbert2@gmail.com>
2024-10-02 23:49:28 +0200

    Add Adjust Timer Method

--- Runtime/Utils/Timer.cs
+++ Runtime/Utils/Timer.cs
@@ -2,7 +2,8 @@
 
 namespace FiveBabbittGames
 { 
-    public abstract class Timer {
+    public abstract class Timer 
+    {
         protected float initialTime;
         protected float Time { get; set; }
         public bool IsRunning { get; protected set; }
@@ -12,24 +13,51 @@ namespace FiveBabbittGames
     
         public Action OnTimerStart = delegate { };
         public Action OnTimerStop = delegate { };
+        public Action<float> OnTimerAdjusted = delegate { };
 
-        protected Timer(float value) {
+        public float GetTime() => Time;
+
+        protected Timer(float value) 
+        {
             initialTime = value;
             IsRunning = false;
         }
 
-        public void Start() {
+        /// <summary>
+        /// Start the <see cref="Timer">, set the current time to the initial time and invoke <see cref="OnTimerStart"/>
+        /// </summary>
+        public void Start()
+        {
             Time = initialTime;
-            if (!IsRunning) {
+            if (!IsRunning) 
+            {
                 IsRunning = true;
                 OnTimerStart.Invoke();
             }
         }
 
-        public void Stop() {
-            if (IsRunning) {
+        /// <summary>
+        /// Stop the timer and invoke <see cref="OnTimerStop"/>
+        /// </summary>
+        public void Stop() 
+        {
+            if (IsRunning) 
+            {
                 IsRunning = false;
                 OnTimerStop.Invoke();
+            }
+        }
+
+        /// <summary>
+        /// Add/Subtract time to <see cref="Time"/> while the timer is running and Invoke <see cref="OnTimerAdjusted"/>
+        /// </summary>
+        /// <param name="time">Time to be added or subtracted</param>
+        public void AdjustTime(float time)
+        {
+            if (IsRunning)
+            {
+                Time += time;
+                OnTimerAdjusted.Invoke(time);
             }
         }
     
@@ -39,15 +67,19 @@ namespace FiveBabbittGames
         public abstract void Tick(float deltaTime);
     }
 
-    public class CountdownTimer : Timer {
+    public class CountdownTimer : Timer 
+    {
         public CountdownTimer(float value) : base(value) { }
 
-        public override void Tick(float deltaTime) {
-            if (IsRunning && Time > 0) {
+        public override void Tick(float deltaTime) 
+        {
+            if (IsRunning && Time > 0) 
+            {
                 Time -= deltaTime;
             }
         
-            if (IsRunning && Time <= 0) {
+            if (IsRunning && Time <= 0) 
+            {
                 Stop();
             }
         }
@@ -55,24 +87,29 @@ namespace FiveBabbittGames
         public bool IsFinished => Time <= 0;
     
         public void Reset() => Time = initialTime;
-    
-        public void Reset(float newTime) {
+
+        /// <summary>
+        /// Overload of <see cref="Reset"/> where initialTime is set to newTime and <see cref="Time"/> is set to initialTime
+        /// </summary>
+        /// <param name="newTime"></param>
+        public void Reset(float newTime) 
+        {
             initialTime = newTime;
             Reset();
         }
     }
 
-    public class StopwatchTimer : Timer {
+    public class StopwatchTimer : Timer 
+    {
         public StopwatchTimer() : base(0) { }
 
-        public override void Tick(float deltaTime) {
+        public override void Tick(float deltaTime) 
+        {
             if (IsRunning) {
                 Time += deltaTime;
             }
         }
     
-        public void Reset() => Time = 0;
-    
-        public float GetTime() => Time;
+        public void Reset() => Time = initialTime;
     }
 }
